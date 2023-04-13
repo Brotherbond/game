f8049c6caafbb421028d7d24bc3f1eea5a7db7ae
Brotherbond <ehinmitankehinde@gmail.com>
2023-04-13 21:56:57 +0100

    Drafted UI basis
    Updated README

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/global.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>BALANCE: <span className="white">XXXX</span></div>
+        <div className='m m2x'>BET: <span className="white">XXX</span></div>
+        <div>WIN: <span className="white">X</span></div>
       </header>
+      <section className='App-main'>
+        <div>
+          <h2 className='green'>PAPER WON</h2>
+          <h4>YOU WIN <span className="white">XXX</span></h4>
+        </div>
+        <h3>
+          <span className="white">ROCK</span>
+          <span>VS</span>
+          <span className="white">PAPER</span>
+
+        </h3>
+
+        <p className='m'>PICK YOUR POSITIONS</p>
+        <div className='flex'>
+          <div className='box box-purple'>
+            <div>500</div>
+            <div>ROCK</div>
+          </div>
+          <div className='box box-green m'>
+            <div>500</div>
+            <div>PAPER</div>
+          </div>
+          <div className='box box-red'>
+            <div className='invisible'>500</div>
+            <div>SCISSORS</div>
+          </div>
+        </div>
+        <div><button>PLAY</button></div>
+      </section>
     </div>
   );
 }
 
 export default App;
+
+
+//find image to html software, 
+//use gpt for best rock, paper, scissor algorithm 
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = (): JSX.Element => {
+
+    return <>
+        <section>
+            <div>BALANCE:XXXX</div>
+            <div>BET:XXX</div>
+            <div>WIN:X</div>
+        </section>
+    </>
+
+}
